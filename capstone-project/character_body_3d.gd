a0ae8d34aaa15510c82b698910f77943876e7073
kdowd9 <kdowd9@fordham.edu>
2026-04-11 23:46:44 -0400

    basic level and character

---
+++ capstone-project/character_body_3d.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const MAX_JUMPS = 3 
+
+var jump_count = 0
+
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		jump_count = 0
+
+	
+	if Input.is_action_just_pressed("ui_accept") and jump_count < MAX_JUMPS:
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+
+
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
