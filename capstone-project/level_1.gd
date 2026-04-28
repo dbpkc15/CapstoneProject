3f63bd1eeee29c8f823e47e4743bbdbc39a857b2
fahmad4 <fahmad4@fordham.edu>
2026-04-27 23:33:56 -0400

    added timer to level 01

--- capstone-project/level_1.gd
+++ capstone-project/level_1.gd
@@ -1,20 +1,18 @@
 extends Node3D
 
-@onready var timer = $Level3Timer
+@onready var timer = $Level1Timer
 @onready var label = $CanvasLayer/Label
 
 var game_over = false
 
 func _ready() -> void:
-	timer.start(30)
+	timer.start(60)
 
 func _process(delta):
 	if not game_over:
-		label.text = "Time: " + str(int(timer.time_left))
+		label.text = "Time: " + str(int(timer.time_left))	
 	
-func _on_level_3_timer_timeout():
+func _on_level_1_timer_timeout():
 	game_over = true
 	label.text = "Time Is Up!"
 	label.visible = true
-	
-	
